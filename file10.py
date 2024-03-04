cbb8b2272d11ab62d8d909828373a16a3e775ea1
Diogo Vinhas <diogo.vinhas.2022207@my.istec.pt>
2024-03-04 10:18:54 +0000

    exercicio 10

---
+++ file10.py
@@ -0,0 +1,11 @@
+# Definindo uma função para calcular a soma de dois números
+def soma(a, b):
+    return a + b
+
+# Solicitando entrada do usuário para os números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Chamando a função soma e imprimindo o resultado
+resultado = soma(num1, num2)
+print("A soma é:", resultado)
